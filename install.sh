5957a40986cc83177f3cae764bca930d8cb38f25
midoks <midoks@163.com>
2018-08-04 00:18:47 +0800

    up

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 EXE_PATH=`pwd`
 
 echo "$EXE_PATH", $EXE_PATH
-sed "s:{{PATH}}:${EXE_PATH}:g" conf/novelsearch.tpl.conf > conf/novelsearch.conf
 
-cp conf/novelsearch.conf /etc/supervisor/
+#sed "s:{{PATH}}:${EXE_PATH}:g" conf/novelsearch.tpl.conf > conf/novelsearch.conf
+#cp conf/novelsearch.conf /etc/supervisor/
+
+sed "s:{{PATH}}:${EXE_PATH}:g" start.tpl.sh > start.sh
+chmod +x start.sh
+
+echo $EXE_PATH/start.sh >> /etc/rc.local
+
